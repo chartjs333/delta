b8a21dd32418eff9991f0a6c291ca73e0d89256f
ds2020ds@mail.ru <ds2020ds@mail.ru>
2026-08-27 16:42:51 +0200

    evidence(T013,T032): bind native ABI boundary

--- specs/003-bft-round-state-machine/scripts/verify_native_supply_chain.py
+++ specs/003-bft-round-state-machine/scripts/verify_native_supply_chain.py
@@ -20,7 +20,7 @@ EXPECTED_TOOLCHAIN_EVIDENCE_SHA256 = (
     "dfc8037582fbbe99539cf635e57e9ae64b41c9c7b2d4e32a47e8fcc347763197"
 )
 EXPECTED_ARCHITECTURE_EVIDENCE_SHA256 = (
-    "7fab8d52a8aea3b79e264d54fb04096125f073900af1ddb7f63a866b3d663615"
+    "b60ca4e68d0053515af5d2648afddb4da41ce2cfe4553542df19a2e00683b13a"
 )
 SOURCE_SUFFIXES = {".c", ".cc", ".cpp", ".cxx", ".h", ".hh", ".hpp", ".java"}
 SOURCE_PREFIXES = ("delta-core-cpp/", "delta-runtime-cpp/", "delta-ffi/", "delta-node-java/")
