42b55f64b811f52c158ed079e38ecd99a6fbcd92
ds2020ds@mail.ru <ds2020ds@mail.ru>
2026-09-15 15:25:36 +0200

    T015 T016 HR010-004: add static model plugin registry and descriptor

--- delta-worker-python/src/deltatorrent/model_plugins/__init__.py
+++ delta-worker-python/src/deltatorrent/model_plugins/__init__.py
@@ -4,11 +4,25 @@ from __future__ import annotations
 
 from deltatorrent.model_plugins.base import EvaluationResult, LocalTrainingResult, ModelPlugin
 from deltatorrent.model_plugins.mnist_centroid import MnistCentroidModel, MnistCentroidPlugin
+from deltatorrent.model_plugins.registry import (
+    MNIST_CENTROID_DESCRIPTOR,
+    ModelPluginRegistry,
+    PluginDescriptor,
+    PluginRegistryError,
+    build_default_registry,
+    get_default_registry,
+)
 
 __all__ = [
+    "MNIST_CENTROID_DESCRIPTOR",
     "EvaluationResult",
     "LocalTrainingResult",
     "MnistCentroidModel",
     "MnistCentroidPlugin",
     "ModelPlugin",
+    "ModelPluginRegistry",
+    "PluginDescriptor",
+    "PluginRegistryError",
+    "build_default_registry",
+    "get_default_registry",
 ]
